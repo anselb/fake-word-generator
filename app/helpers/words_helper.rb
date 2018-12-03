9401252ec8cc0f9613655f234566fb65671105ca
Ansel Bridgewater <iamansel@gmail.com>
2018-12-02 23:48:43 -0800

    Created nth_markov_dictograms method

--- app/helpers/words_helper.rb
+++ app/helpers/words_helper.rb
@@ -54,4 +54,30 @@ module WordsHelper
     end
 
   end
+
+  def nth_markov_dictograms(text_array, nth_order)
+    dictogram_hash = Hash.new
+
+    for word in text_array
+      if word.length <= nth_order
+        next
+      else
+        last_index = text_array.length - nth_order
+        for char_index in 0...last_index
+          nth_char = char_index + nth_order
+          current_letters = word[char_index...nth_char]
+          next_letter = word[char_index + nth_order]
+
+          if dictogram_hash.key?(current_letters)
+            dictogram_hash[current_letters].add_count(next_letter)
+          else
+            dictogram_hash[current_letters] = Dictogram.new(next_letter)
+          end
+
+        end
+      end
+    end
+
+    dictogram_hash
+  end
 end
