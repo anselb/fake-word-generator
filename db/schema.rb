00c0c26103d1dbfd87652d34bc843e091e7b696d
Ansel Bridgewater <iamansel@gmail.com>
2018-12-06 14:18:28 -0800

    Converted database from sqlite3 to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_12_03_012259) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "words", force: :cascade do |t|
     t.string "word"
     t.integer "votes"
