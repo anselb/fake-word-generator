e6dca16c70afaf183ea654b1cbf1ae09c08e0ea5
Ansel Bridgewater <iamansel@gmail.com>
2018-12-14 01:13:27 -0800

    Fixed array type in migrations and ran migrations

--- db/migrate/20181214085956_create_component_lists.rb
+++ db/migrate/20181214085956_create_component_lists.rb
@@ -2,7 +2,7 @@ class CreateComponentLists < ActiveRecord::Migration[5.2]
   def change
     create_table :component_lists do |t|
       t.text :components
-      t.array :components_array
+      t.text :components_array, array: true
 
       t.timestamps
     end
--- db/migrate/20181214090057_create_order_lists.rb
+++ db/migrate/20181214090057_create_order_lists.rb
@@ -2,7 +2,7 @@ class CreateOrderLists < ActiveRecord::Migration[5.2]
   def change
     create_table :order_lists do |t|
       t.text :order
-      t.array :order_array
+      t.text :order_array, array: true
 
       t.timestamps
     end
